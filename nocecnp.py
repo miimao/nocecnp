5391e21ec54a0ae85f89edbd2cdc4dac0cb44b13
miimao <josephgaumond@hotmail.com>
2025-07-31 20:35:11 -0600

    Added Systemd DBus Watching for sleep signals and example config

--- nocecnp.py
+++ nocecnp.py
@@ -1,8 +1,11 @@
 from nocecnp.config import read_config
-
-import sys
-import requests
+import asyncio
 from time import sleep, time
+import asyncio
+from dbus_next.aio.message_bus import MessageBus
+from dbus_next.constants import BusType
+from dbus_next.constants import MessageType
+from dbus_next.signature import Variant
 
 def setup_display(config):
     display_make = config.display_make
@@ -16,14 +19,28 @@ def setup_display(config):
         case _:
             raise ValueError(f"Unsupported display make: {display_make}")
 
+async def watch_sleep_signals(display):
+    bus = await MessageBus(bus_type=BusType.SYSTEM).connect()
+    introspection = await bus.introspect('org.freedesktop.login1', '/org/freedesktop/login1')
+    obj = bus.get_proxy_object('org.freedesktop.login1', '/org/freedesktop/login1', introspection)
+    manager = obj.get_interface('org.freedesktop.login1.Manager')
+
+    def on_prepare_for_sleep(sleeping):
+        if sleeping:
+            print("System is going to sleep, powering off display.")
+            display.power_off()
+        else:
+            print("System woke up, powering on display.")
+            display.power_on()
+
+    manager.on_prepare_for_sleep(on_prepare_for_sleep)
+    await asyncio.get_event_loop().create_future()  # Run forever
+
 def main():
     config = read_config()
     print(config)
     display = setup_display(config)
-
-    display.power_on()
-    sleep(10)
-    display.power_off()
+    asyncio.run(watch_sleep_signals(display))
 
 if __name__ == "__main__":
     main()
